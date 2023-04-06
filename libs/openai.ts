77fe9e32707d8c3748055bc3a1e7d448f187bb6c
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-04-07 07:44:40 +0900

    formatted

--- libs/openai.ts
+++ libs/openai.ts
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv';
-import { Configuration, OpenAIApi, CreateCompletionRequest } from 'openai';
+import dotenv from 'dotenv'
+import { Configuration, OpenAIApi } from 'openai'
 
-dotenv.config();
+dotenv.config()
 
 const configuration = new Configuration({
   organization: process.env.OPENAI_ORGANIZATION,
   apiKey: process.env.OPENAI_API_KEY,
-});
+})
 
-export const openai = new OpenAIApi(configuration);
+export const openai = new OpenAIApi(configuration)
