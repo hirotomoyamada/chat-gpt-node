c82cc8b5be120caa83fd9083fb647f571c73f74d
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-04-12 13:19:34 +0900

    add questionSync

--- libs/readline.ts
+++ libs/readline.ts
@@ -1,6 +1,25 @@
-import readline from 'readline';
+import readline from 'readline'
+import { red } from 'chalk'
 
 export const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+})
+
+export const questionSync = async (
+  query: string,
+  callback?: (value: string) => void | Promise<void>,
+) =>
+  new Promise((resolve) =>
+    rl.question(query, async (value) => {
+      try {
+        await callback?.(value)
+
+        resolve(value)
+      } catch (e) {
+        console.error('\n' + red(e.message) + '\n')
+
+        rl.close()
+      }
+    }),
+  )
