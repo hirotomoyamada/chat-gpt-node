957e63cc807b84496b137d097b087b7d158420aa
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-04-13 14:31:10 +0900

    specify the dir

--- utils/json.ts
+++ utils/json.ts
@@ -5,29 +5,31 @@ import { prettier } from '../libs/prettier'
 
 const writeFileAsync = promisify(writeFile)
 
-export const JSON_DIR_PATH = path.join(process.cwd(), 'json')
-export const JSON_FILE_PATH = (name: string) => path.join(process.cwd(), 'json', `${name}.json`)
+export const JSON_DIR_PATH = (dirName: string) => path.join(process.cwd(), dirName)
+export const JSON_FILE_PATH = (dirName: string, fileName: string) =>
+  path.join(process.cwd(), dirName, `${fileName}.json`)
 
-export const existsJsonDir = () => existsSync(JSON_DIR_PATH)
+export const existsJsonDir = (dirName: string) => existsSync(JSON_DIR_PATH(dirName))
 
-export const writeJson = async (name: string, data: any): Promise<void> => {
+export const writeJson = async (dirName: string, fileName: string, data: any): Promise<void> => {
   try {
-    if (!existsJsonDir()) mkdirSync(JSON_DIR_PATH)
+    if (!existsJsonDir(dirName)) mkdirSync(JSON_DIR_PATH(dirName))
 
     const formattedData = await prettier(JSON.stringify(data))
 
-    await writeFileAsync(JSON_FILE_PATH(name), formattedData, 'utf8')
+    await writeFileAsync(JSON_FILE_PATH(dirName, fileName), formattedData, 'utf8')
   } catch (e) {
     throw new Error(e.message)
   }
 }
 
-export const readJson = (name: string): any => {
+export const readJson = (dirName: string, fileName: string): any => {
   try {
-    return JSON.parse(readFileSync(JSON_FILE_PATH(name), 'utf8'))
+    return JSON.parse(readFileSync(JSON_FILE_PATH(dirName, fileName), 'utf8'))
   } catch (e) {
     throw new Error(e.message)
   }
 }
 
-export const existsJson = (name: string): boolean => existsSync(JSON_FILE_PATH(name))
+export const existsJson = (dirName: string, fileName: string): boolean =>
+  existsSync(JSON_FILE_PATH(dirName, fileName))
