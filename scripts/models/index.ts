69fe3af6709956081ff8b187b5d514e1c9433e50
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-04-14 03:44:53 +0900

    formatted

--- scripts/models/index.ts
+++ scripts/models/index.ts
@@ -1,38 +1,60 @@
 import ora from 'ora'
 import { openai } from '../../libs/openai'
-import { red } from 'chalk'
-import { computedCommnad, readJson } from '../../utils'
+import { red, green, dim } from 'chalk'
+import { computedCommnad } from '../../utils/command'
+import { existsJson, readJson } from '../../utils/json'
+import { DefinedModel, InternalModel } from '../../types/model'
 
-const fetchModels = async () => {
-  const { remote } = computedCommnad(['remote'])
+const defaultMessages = (models: string[]) =>
+  `\n${models.map((model, i) => `${(i + 1).toString().padStart(2, '0')}. ${model}`).join('\n')}\n`
 
-  const spinner = ora('Getting models for ChatGPT').start()
+export const main = async () => {
+  const command = computedCommnad(['internal', 'remote', 'parameters'])
+
+  const spinner = ora(dim('Getting models for ChatGPT...')).start()
 
   try {
-    let models: string[] = []
+    let messages: string = ''
 
-    if (remote) {
+    if (command.internal) {
+      const data: InternalModel[] = readJson('json', 'models')
+
+      const models: string[] = data.map(({ id }) => id)
+
+      messages = defaultMessages(models)
+    } else if (command.remote) {
       const { data } = await openai.listModels()
 
-      models = data.data.map(({ id }) => id)
+      const models: string[] = data.data.map(({ id }) => id)
+
+      messages = defaultMessages(models)
     } else {
-      models = Object.keys(readJson('models'))
+      const data: DefinedModel[] = existsJson('data', 'models') ? readJson('data', 'models') : []
+
+      if (data.length) {
+        if (!command.parameters) {
+          const models: string[] = data.map(({ id }) => id)
+
+          messages = defaultMessages(models)
+        } else {
+        }
+      } else {
+        throw new Error(
+          'Model does not exist. Please run "pnpm model --create" to create the model.',
+        )
+      }
     }
 
-    spinner.succeed('Got models')
+    spinner.succeed(green('Got models.'))
 
-    const result = `\n${models
-      .map((model, i) => `${(i + 1).toString().padStart(2, '0')}. ${model}`)
-      .join('\n')}\n`
-
-    console.log(result)
+    console.log(messages)
   } catch (e) {
-    spinner.fail(red('An error occurred'))
+    spinner.fail(red('An error occurred.'))
 
-    console.error(red(e.message))
+    console.error('\n' + red(e.message) + '\n')
   } finally {
     spinner.stop()
   }
 }
 
-fetchModels()
+main()
